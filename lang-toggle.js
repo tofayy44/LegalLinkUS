6ef25edad0a952fae42045106887432fb8506915
tofayy44 <beendidthat.co@gmail.com>
2025-05-12 15:20:32 -0700

    Delete lang-toggle.js

--- lang-toggle.js
+++
@@ -1,6 +0,0 @@
-
-function toggleLanguage() {
-    const lang = document.documentElement.lang;
-    document.documentElement.lang = (lang === 'en') ? 'es' : 'en';
-    alert("Language switched to " + document.documentElement.lang.toUpperCase());
-}
