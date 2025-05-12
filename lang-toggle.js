31b75135e5663c72113d202f03360e24c88a245b
tofayy44 <beendidthat.co@gmail.com>
2025-05-12 13:43:29 -0700

    Delete lang-toggle.js

--- lang-toggle.js
+++
@@ -1,8 +0,0 @@
-
-function toggleLanguage() {
-    const elements = document.querySelectorAll('[data-en]');
-    const isSpanish = document.body.classList.toggle('spanish');
-    elements.forEach(el => {
-        el.innerText = isSpanish ? el.dataset.es : el.dataset.en;
-    });
-}
