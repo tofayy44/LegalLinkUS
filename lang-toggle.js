652dcb366044d9638dd14f83a4297ae02a7a71f4
tofayy44 <beendidthat.co@gmail.com>
2025-05-12 18:49:51 -0700

    Add files via upload

--- lang-toggle.js
+++ lang-toggle.js
@@ -1,3 +1,16 @@
-function toggleLanguage() {
-  alert("Language toggle coming soon!");
-}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const enSection = document.getElementById("rights-en");
+  const esSection = document.getElementById("rights-es");
+  const toggleBtn = document.getElementById("langToggleBtn");
+
+  toggleBtn.addEventListener("click", () => {
+    if (enSection.style.display === "none") {
+      enSection.style.display = "block";
+      esSection.style.display = "none";
+    } else {
+      enSection.style.display = "none";
+      esSection.style.display = "block";
+    }
+  });
+});
